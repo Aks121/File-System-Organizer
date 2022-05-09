8f1b8a7c6154f058b695457fd27ad4e97ca9312b
Akanksha Srivastava <79524764+Aks121@users.noreply.github.com>
2022-05-09 21:58:41 +0530

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,8 @@
+let utility = {}
+utility.types = {
+    media: ["mp4", "mkv"],
+    archives: ['zip', '7s', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
+    documents: ['docx', 'doc', 'pdf', 'xlsx', 'xls', 'odt', 'ods', 'odp', 'odg', 'odf', 'txt', 'ps', 'tex' ],
+    app: ['exe', 'dng', 'pkg', 'deb']
+}
+module.exports = utility;
